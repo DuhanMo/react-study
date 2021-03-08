1cd39bd81eb26bfbb23aaff8f2acb9fd0655a827
DuhanMo <devduhanmo@gmail.com>
2021-03-08 14:34:57 +0900

    useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import reactDom from 'react-dom';
+import { useState } from 'react';
+import Sub from './Sub';
 
 // 0. React 엔진 - 데이터 변경감지해서 UI 그려주는 엔진
 // 1. 실행과정 (index.html) -SPA
@@ -30,16 +32,58 @@ import reactDom from 'react-dom';
 //   );
 // }
 
-function App() {
-  let list = [1, 2, 3];
+// function App() {
+//   let list = [1, 2, 3];
 
+//   // let number = 1; // 상태변수 , 상태값은 아님
+//   // 상태값으로 만든 후 리액트엔진이 관리하게 하려면
+//   const [number, setNumber] = useState(1); // React안에 hooks 라이브러리를 쓴 것. 상태값이 됨. UI와 동기화가 되어있음
+//   const add = () => {
+//     // number++ 사용불가 , setNumber로만 number변수 제어 가능
+//     setNumber(number + 1); // React한테 'number값 변경할게' 라고 요청하는 것.
+//     console.log('add', number);
+//   };
+//   const init = () => {
+//     setNumber(0);
+//     console.log('init', number);
+//   };
+//   // 랜더링 시점 = 상태값 변경될 때
+//   return (
+//     <div>
+//       <h1>숫자: {number}</h1>
+//       {/* 함수를 넣을때는 바인딩만 한다 ( ()를 넣으면 안됨 ) */}
+//       <button onClick={add}>더하기</button>
+//       <Sub />
+//       <br></br>
+//       <button onClick={init}>초기화</button>
+//     </div>
+//   );
+// }
+
+// 문제
+function App() {
+  console.log('App 실행됨');
+  // 다운로드 받음
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '장보고' },
+    { id: 3, name: '궁예' },
+    { id: 4, name: '견원' },
+  ];
+  const [users, setUsers] = useState(sample); // 레퍼런스 변경되야 동작!
+  const download = () => {
+    sample.push({ id: 5, name: '조자룡' });
+    console.log(sample);
+    setUsers([...sample]);
+  };
   return (
     <div>
-      <div>
-        {list.map((n) => (
-          <h1>{n}</h1>
-        ))}
-      </div>
+      <button onClick={download}>다운로드</button>
+      {users.map((u) => (
+        <l1>
+          {u.id} : {u.name},
+        </l1>
+      ))}
     </div>
   );
 }
