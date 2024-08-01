b9d80b526fe05d949c88db6a358ac90c3c067385
Duhan Mo <duhandv@gmail.com>
2024-08-01 23:07:15 +0900

    [shop] feat: Route를 활용한 페이지 이동

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
+import { useState } from "react";
+import data from "./data";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import  Detail  from "./routes/Detail.js"
 
 function App() {
+  let [shoesList] = useState(data);
+  let navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+                {
+                  shoesList.map((shoes, i) => {
+                    return (
+                      <Card shoes={shoes} i={i} />
+                    );
+                  })
+                }
+              </div>
+            </div>
+          </>
+        }></Route>
+        <Route path="/detail" element={<Detail/>}></Route>
+        <Route path="/about" element={<div>어바웃페이지</div>}></Route>
+        <Route path="*" element={<div>404</div>}></Route>
+      </Routes>
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
+      <Link to="/about">어바웃페이지</Link>
+      <Button className="primary"onClick={()=>navigate('detail')}>디테일 이동버튼</Button>
     </div>
   );
 }
-
 export default App;
+
+function Card(props) {
+  return <div className="col-md4" key={props.i}>
+    <img src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`} width="80%" />
+    <h4>{props.shoes.title}</h4>
+    <p>{props.shoes.content}</p>
+  </div>;
+}
+
